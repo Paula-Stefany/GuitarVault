25b68a07e823a5dadfd195cfc296d35395ed4f5b
Paula-Stefany <paula.sfa@outlook.com>
2024-09-12 14:41:40 -0300

    criação menu mobile

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const bars = document.getElementById("bars");
+const nav = document.getElementById("navigation")
+
+function toogleMenu() {
+    nav.classList.toggle('active');
+}
+
+bars.addEventListener("click", toogleMenu);
