8ab51c4e82d4f6f92a829722bc8e8e8b5de64e76
Paula-Stefany <paula.sfa@outlook.com>
2024-09-17 18:23:25 -0300

    adicionando eventos nas setas + refatorações

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,53 @@ const bars = document.getElementById("bars");
 const nav = document.getElementById("navigation")
 
 function toogleMenu() {
+    
     nav.classList.toggle('active');
 }
 
 bars.addEventListener("click", toogleMenu);
+
+
+/* PRODUCT CARDS 1*/
+
+const card = document.getElementById("product-cards");
+const scroollLeft = document.getElementById("left")
+const scroollRight = document.getElementById("right");
+
+
+scroollLeft.addEventListener("click", () => {
+    
+    card.scrollBy({
+        left: - 270,
+        behavior: "smooth",
+    });
+});
+
+scroollRight.addEventListener("click", () => {
+    card.scrollBy({
+        left: 270,
+        behavior: "smooth",
+    });
+});
+
+/* PRODUCT CARDS 2 */
+
+const card2 = document.getElementById("product-cards2");
+
+const scroollLeft2 = document.getElementById("left2");
+
+const scroollRight2 = document.getElementById("right2");
+
+scroollLeft2.addEventListener("click", () => {
+    card2.scrollBy({
+        left: - 270,
+        behavior: "smooth",
+    })
+})
+
+scroollRight2.addEventListener("click", () => {
+    card2.scrollBy({
+        left: 270,
+        behavior: "smooth",
+    });
+});
